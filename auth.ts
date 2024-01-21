4e694a19f93ee2cd155de6db3cbf0b8fe0a6b2dc
chrisprb <chrismb774@outlook.com>
2024-01-21 13:31:47 -0500

    General improvements

--- auth.ts
+++ auth.ts
@@ -1,21 +1,21 @@
-import NextAuth from 'next-auth';
-import { authConfig } from './auth.config';
-import Credentials from 'next-auth/providers/credentials';
-import { z } from 'zod';
+// import NextAuth from 'next-auth';
+// import { authConfig } from './auth.config';
+// import Credentials from 'next-auth/providers/credentials';
+// import { z } from 'zod';
  
 
 
 
-export const { auth, signIn, signOut } = NextAuth({
-  ...authConfig,
-  providers: [
-    Credentials({
-      //@ts-ignore
-      async authorize(credentials) {
-        const parsedCredentials = z
-          .object({ email: z.string().email(), password: z.string().min(6) })
-          .safeParse(credentials);
-      },
-    }),
-  ],
-});
+// export const { auth, signIn, signOut } = NextAuth({
+//   ...authConfig,
+//   providers: [
+//     Credentials({
+//       //@ts-ignore
+//       async authorize(credentials) {
+//         const parsedCredentials = z
+//           .object({ email: z.string().email(), password: z.string().min(6) })
+//           .safeParse(credentials);
+//       },
+//     }),
+//   ],
+// });
